6734f12451a892a15585bf1593398a62872da3d3
brajeshepik <brajesh@epikso.com>
2025-05-11 17:07:39 +0530

    update the index and app file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Node');
+  res.send('Hello Node, This is the ECS deployenmt with node js');
 });
 
 app.listen(8080, () => {
